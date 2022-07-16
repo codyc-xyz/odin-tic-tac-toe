d183e8807ed3ae931e0ec2622ae98c689d0a05be
Red <crabmarket56@gmail.com>
2022-07-16 15:18:05 -0400

    Fix styling

--- script.rb
+++ script.rb
@@ -2,45 +2,47 @@
 require 'pry-byebug'
 
 class Board
-  def initialize (rows = 3, columns = 3)
-  $game_board = Array.new(rows) {Array.new(columns)}
-  p $game_board
+  def initialize(rows = 3, columns = 3)
+    $game_board = Array.new(rows) { Array.new(columns) }
+    p $game_board
   end
 end
 
 class Player
-attr_accessor :name, :symbol, :row, :column
+  attr_accessor :name, :symbol, :row, :column
 
-  def initialize (name, symbol)
-  @name = name
-  @symbol = symbol
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
   end
- 
+
   def select_row
     p "#{@name} it's your turn! Please input your desired row and press enter"
-      @row = gets.chomp.to_i
-      if @row < 3 && @row >= 0
+    @row = gets.chomp.to_i
+    if @row < 3 && @row >= 0
       @row = row
-    else self.select_row
+    else
+      select_row
     end
   end
 
   def select_column
     p "great choice #{@name}! Next, please enter your desired column and press enter"
-      @column = gets.chomp.to_i
-      if @column < 3 && @column >= 0
+    @column = gets.chomp.to_i
+    if @column < 3 && @column >= 0
       @column = column
-    else self.select_column
+    else
+      select_column
     end
   end
 
   def place_symbol(row, column)
-  if $game_board[row][column] == nil 
-  $game_board[row][column] = @symbol
-  p $game_board
-  else
-    self.play_round
-  end
+    if $game_board[row][column].nil?
+      $game_board[row][column] = @symbol
+      p $game_board
+    else
+      play_round
+    end
   end
 
   def winner?
@@ -51,19 +53,19 @@ attr_accessor :name, :symbol, :row, :column
        $game_board[0][1] == @symbol && $game_board[1][1] == @symbol && $game_board[2][1] == @symbol ||
        $game_board[0][2] == @symbol && $game_board[1][2] == @symbol && $game_board[2][2] == @symbol ||
        $game_board[0][0] == @symbol && $game_board[1][1] == @symbol && $game_board[2][2] == @symbol ||
-       $game_board[2][0] == @symbol && $game_board[1][1] == @symbol && $game_board[0][2] == @symbol 
+       $game_board[2][0] == @symbol && $game_board[1][1] == @symbol && $game_board[0][2] == @symbol
 
-       p "Congratulations #{@name}! You are the winner! The board will now reset if you would like to play again!"
-       p $game_board = Board.new
-   end
+      p "Congratulations #{@name}! You are the winner! The board will now reset if you would like to play again!"
+      p $game_board = Board.new
+    end
   end
- 
-def play_round
-  self.select_row()
-  self.select_column()
-  self.place_symbol(row, column)
-  self.winner?
-end
+
+  def play_round
+    select_row
+    select_column
+    place_symbol(row, column)
+    winner?
+  end
 end
 
 Board.new
@@ -71,6 +73,6 @@ p johnny = Player.new('Johnny', 'X')
 p bob = Player.new('Bob', 'O')
 
 for i in 0...$game_board.length
-johnny.play_round()
-bob.play_round()
+  johnny.play_round
+  bob.play_round
 end
