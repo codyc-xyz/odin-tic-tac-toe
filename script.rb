caf6fa9886d677f5a161345ffcf01a8a745abce1
Red <crabmarket56@gmail.com>
2022-07-16 10:01:10 -0400

    Add board alteration module

--- script.rb
+++ script.rb
@@ -1,29 +1,35 @@
 # players take turns placing either an X or an O until one gets three in a row/across or the board is filled
 
-board = [[nil, nil, nil],
-         [nil, nil, nil],
-         [nil, nil, nil]]
+$board = [[Array.new(3) { Array.new(3)}]]
 
-class PlayerOneMove
-  include GetInput
-  def initialize(vertical, horizontal)
+module AlterBoard
+  def place_move(vertical, horizontal)
     @vertical = vertical
     @horizontal = horizontal
-    board[vertical, horizontal] = 'X'
+    $board[vertical.to_i, horizontal.to_i] = 'X'
   end
 end
 
-class PlayerTwoMove
-  include GetInput
-  def initialize(vertical, horizontal)
-    @vertical = vertical
-    @horizontal = horizontal
-    board[vertical, horizontal] = 'O'
-  end
+class Player 
+  include AlterBoard
+end
+class PlayerOneMove < Player
 end
 
-for i in 0...board.length
-  PlayerOneMove.new(vertical = gets.chomp, horizontal = gets.chomp)
-  PlayerTwoMove.new(vertical = gets.chomp, horizontal = gets.chomp)
-  p board
+class PlayerTwoMove < Player
+ def place_move(vertical,horizontal)
+  @vertical = vertical
+  @horizontal = horizontal
+  $board[vertical.to_i, horizontal.to_i] = 'O'
+ end
+end
+
+for i in 0...5
+  p1_move = PlayerOneMove.new
+  p1_move.place_move(vertical = gets.chomp, horizontal = gets.chomp)
+
+  p2_move = PlayerTwoMove.new
+  p2_move.place_move(vertical = gets.chomp, horizontal = gets.chomp)
+
+  p $board
 end
