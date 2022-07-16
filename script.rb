297d6f72906c9869982e8da3b1403478499d3834
Red <crabmarket56@gmail.com>
2022-07-16 09:15:03 -0400

    Draft loop logic

--- script.rb
+++ script.rb
@@ -1,15 +1,14 @@
-#players take turns placing either an X or an O until one gets three in a row/across or the board is filled
+# players take turns placing either an X or an O until one gets three in a row/across or the board is filled
 
 board = [[nil, nil, nil],
-        [nil, nil, nil],
-        [nil, nil, nil]]
-
+         [nil, nil, nil],
+         [nil, nil, nil]]
 
 class PlayerOneMove
   def initialize(vertical, horizontal)
     @vertical = vertical
     @horizontal = horizontal
-    board[vertical, horizontal] = "X"
+    board[vertical, horizontal] = 'X'
   end
 end
 
@@ -17,11 +16,12 @@ class PlayerTwoMove
   def initialize(vertical, horizontal)
     @vertical = vertical
     @horizontal = horizontal
-    board[vertical, horizontal] = "O"
+    board[vertical, horizontal] = 'O'
   end
 end
 
-PlayerOneMove.new()
-
-PlayerTwoMove.new()
-
+for i in 0...board.length
+  PlayerOneMove.new()
+  PlayerTwoMove.new()
+  p board
+end
