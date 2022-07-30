4a6ae47306f95733e9a4b3cde27a8ec336fa8a62
Red <crabmarket56@gmail.com>
2022-07-30 15:57:13 -0400

    Refactor test and method to remove repetition

--- spec/tic-tac-toe-spec.rb
+++ spec/tic-tac-toe-spec.rb
@@ -1,24 +1,24 @@
 require '../tic-tac-toe.rb'
 
-describe TicTacToe do 
-  subject(:player_one_row) {described_class.new('Johnny', 'X', 'Bob', 'O')}
+describe Player do 
+  subject(:row) {described_class.new('Johnny', 'X')}
   
-  describe '#player_one_select_row' do
+  describe 'select_row' do
   context 'when user inputs an invalid row choice and then a valid row choice' do
     before do
       invalid = 'a'
       valid = 2
-      allow(player_one_row).to receive(:gets).and_return(invalid, valid)
+      allow(row).to receive(:gets).and_return(invalid, valid)
     end
       it 'displays a turn message twice, an error message once, and the selection message once' do
-        name = player_one_row.instance_variable_get(:@player_one)
+        name = row.instance_variable_get(:@name)
         turn_message = "#{name} it's your turn! Please input your desired row (1-3) and press enter"
         error_message = "that's an invalid choice #{name}, please input a number between 1-3"
         selection_message = "great choice #{name}, you have selected 2"
-        expect(player_one_row).to receive(:puts).with(turn_message).twice
-        expect(player_one_row).to receive(:puts).with(error_message).once
-        expect(player_one_row).to receive(:puts).with(selection_message).once
-        player_one_row.player_one_select_row
+        expect(row).to receive(:puts).with(turn_message).twice
+        expect(row).to receive(:puts).with(error_message).once
+        expect(row).to receive(:puts).with(selection_message).once
+        row.select_row
       end
     end
 
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,26 +1,44 @@
+class Player
+  attr_reader :name, :sybmol
+  attr_accessor :row, :column
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+    @row 
+    @column
+  end
+
+  def select_row
+    puts "#{self.name} it's your turn! Please input your desired row (1-3) and press enter"
+    @row = gets.to_i
+      if self.row < 4 && self.row > 0
+        puts "great choice #{self.name}, you have selected #{@row}"
+        @row -= 1 
+      else
+        puts "that's an invalid choice #{self.name}, please input a number between 1-3"
+        select_row
+      end
+
+  end
+
+end
+
 class TicTacToe
-  attr_reader :player_one, :player_one_symbol, :player_two, :player_two_symbol
+  attr_reader :player_one, :player_two
   attr_accessor :board
   
-  def initialize(player_one, player_one_symbol, player_two, player_two_symbol)
-    @player_one = player_one
-    @player_one_symbol = player_one_symbol
-    @player_two = player_two
-    @player_two_symbol = player_two_symbol
+  def initialize
+    @player_one = Player.new('Johnny', 'X')
+    @player_two = Player.new('Bob', '0')
     @board = Array.new(3) {Array.new(3)}
   end
 
   def player_one_select_row
-    puts "#{@player_one} it's your turn! Please input your desired row (1-3) and press enter"
-    player_one_row = gets.to_i
-      if player_one_row < 4 && player_one_row > 0
-        puts "great choice #{@player_one}, you have selected #{player_one_row}"
-        player_one_row -= 1 
-      else
-        puts "that's an invalid choice #{@player_one}, please input a number between 1-3"
-        player_one_select_row
-      end
+    @player_one.select_row
+    
   end
+
+   
 end
 
 
