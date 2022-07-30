10f042e248e079cf93f5e264c4c841be8e23406e
Red <crabmarket56@gmail.com>
2022-07-30 15:31:46 -0400

    Complete first row selection test and code pair

--- spec/tic-tac-toe-spec.rb
+++ spec/tic-tac-toe-spec.rb
@@ -1,38 +1,27 @@
 require '../tic-tac-toe.rb'
 
 describe TicTacToe do 
-  subject(:row_input) {described_class.new('Johnny', 'X')}
+  subject(:player_one_row) {described_class.new('Johnny', 'X', 'Bob', 'O')}
   
-  describe '#select_row' do
-  context 'when user inputs an incorrect row choice' do
+  describe '#player_one_select_row' do
+  context 'when user inputs an invalid row choice and then a valid row choice' do
     before do
       invalid = 'a'
-      allow(row_input).to receive(:gets).and_return(invalid)
-    end
-      it 'displays a turn message once and an error message once' do
-        name = row_input.instance_variable_get(:@name)
-        turn_message = "#{name} it's your turn! Please input your desired row (0-2) and press enter"
-        error_message = "that's an invalid choice #{name}, please input a number between 0-2"
-        expect(row_input).to receive(:puts).with(turn_message).once
-        expect(row_input).to receive(:puts).with(error_message).once
-        row_input.select_row
-      end
-    end
-
-  context 'when user inputs correct row choice' do
-    before do
       valid = 2
-      allow(row_input).to receive(:gets).and_return(valid)
-      end
-
-      it 'displays a turn message once and the selection message once' do
-      name = row_input.instance_variable_get(:@name)
-      turn_message = "#{name} it's your turn! Please input your desired row (0-2) and press enter"
-      selection_message = "great choice #{name}, you have selected 2"
-      expect(row_input).to receive(:puts).with(turn_message).once
-      expect(row_input).to receive(:puts).with(selection_message).once
-      row_input.select_row
+      allow(player_one_row).to receive(:gets).and_return(invalid, valid)
+    end
+      it 'displays a turn message twice, an error message once, and the selection message once' do
+        name = player_one_row.instance_variable_get(:@player_one)
+        turn_message = "#{name} it's your turn! Please input your desired row (1-3) and press enter"
+        error_message = "that's an invalid choice #{name}, please input a number between 1-3"
+        selection_message = "great choice #{name}, you have selected 2"
+        expect(player_one_row).to receive(:puts).with(turn_message).twice
+        expect(player_one_row).to receive(:puts).with(error_message).once
+        expect(player_one_row).to receive(:puts).with(selection_message).once
+        player_one_row.player_one_select_row
       end
     end
+
+  
   end
 end
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -8,7 +8,19 @@ class TicTacToe
     @player_two = player_two
     @player_two_symbol = player_two_symbol
     @board = Array.new(3) {Array.new(3)}
-
   end
 
+  def player_one_select_row
+    puts "#{@player_one} it's your turn! Please input your desired row (1-3) and press enter"
+    player_one_row = gets.to_i
+      if player_one_row < 4 && player_one_row > 0
+        puts "great choice #{@player_one}, you have selected #{player_one_row}"
+        player_one_row -= 1 
+      else
+        puts "that's an invalid choice #{@player_one}, please input a number between 1-3"
+        player_one_select_row
+      end
+  end
 end
+
+
