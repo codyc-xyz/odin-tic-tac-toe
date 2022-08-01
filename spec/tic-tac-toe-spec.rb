e28dec2c1713deec91b8861a01a9645efd70b5b1
Red <crabmarket56@gmail.com>
2022-08-01 14:20:42 -0400

    Clean styling

--- spec/tic-tac-toe-spec.rb
+++ spec/tic-tac-toe-spec.rb
@@ -1,14 +1,14 @@
-require '../tic-tac-toe.rb'
+require '../tic-tac-toe'
 
-describe Player do   
+describe Player do
   describe 'select_row' do
-    subject(:row) {described_class.new('Johnny', 'X')}
-  context 'when user inputs an invalid row choice and then a valid row choice' do
-    before do
-      invalid = 'a'
-      valid = 2
-      allow(row).to receive(:gets).and_return(invalid, valid)
-    end
+    subject(:row) { described_class.new('Johnny', 'X') }
+    context 'when user inputs an invalid row choice and then a valid row choice' do
+      before do
+        invalid = 'a'
+        valid = 2
+        allow(row).to receive(:gets).and_return(invalid, valid)
+      end
       it 'displays a turn message twice, an error message once, and the selection message once' do
         name = row.instance_variable_get(:@name)
         turn_message = "#{name} it's your turn! Please input your desired row (1-3) and press enter"
@@ -23,7 +23,7 @@ describe Player do
   end
 
   describe 'select_column' do
-    subject(:column) {described_class.new('Johnny', 'X')}
+    subject(:column) { described_class.new('Johnny', 'X') }
 
     context 'when user inputs an invalid column choice and then a valid column choice' do
       before do
@@ -42,15 +42,14 @@ describe Player do
         column.select_column
       end
     end
-   end
+  end
 end
 
 describe TicTacToe do
   describe 'place_symbol' do
-    subject(:game_board) {described_class.new}
+    subject(:game_board) { described_class.new }
 
     context 'when user has submitted a valid row and column selection and that board position is empty' do
-    
       it 'changes the user\'s chosen board position from nil to the user\'s symbol' do
         row = 2
         column = 2
@@ -58,39 +57,37 @@ describe TicTacToe do
         board = game_board.instance_variable_get(:@board)
         expect(board[row][column]).to eq(nil)
         game_board.place_symbol(row, column, symbol)
-        expect(board[row][column]).to eq(symbol)     
+        expect(board[row][column]).to eq(symbol)
       end
     end
 
     context 'when chosen board position is not empty' do
-
       it 'does not change the board state' do
-      row = 2
-      column = 2
-      symbol = game_board.instance_variable_get(:@player_one_symbol)
-      board = game_board.instance_variable_get(:@board)
-      board[row][column]= 'O'
-      game_board.place_symbol(row, column, symbol, board)
-      expect(board[row][column]).to eq('O')
-    end
+        row = 2
+        column = 2
+        symbol = game_board.instance_variable_get(:@player_one_symbol)
+        board = game_board.instance_variable_get(:@board)
+        board[row][column] = 'O'
+        game_board.place_symbol(row, column, symbol, board)
+        expect(board[row][column]).to eq('O')
+      end
 
-    it 'returns an error message and prints the board' do
-      row = 2
-      column = 2
-      symbol = game_board.instance_variable_get(:@player_one_symbol)
-      board = game_board.instance_variable_get(:@board)
-      board[row][column]= 'O'
-      error_message = "That board position is already full, please select an empty position\n#{board}\n"
-      expect{game_board.place_symbol(row, column, symbol, board)}.to output(error_message).to_stdout
+      it 'returns an error message and prints the board' do
+        row = 2
+        column = 2
+        symbol = game_board.instance_variable_get(:@player_one_symbol)
+        board = game_board.instance_variable_get(:@board)
+        board[row][column] = 'O'
+        error_message = "That board position is already full, please select an empty position\n#{board}\n"
+        expect { game_board.place_symbol(row, column, symbol, board) }.to output(error_message).to_stdout
+      end
     end
   end
-end
 
   describe 'winner?' do
-    subject(:game_board) {described_class.new}
+    subject(:game_board) { described_class.new }
 
     context 'when a user has completed a horizontal row of three symbols' do
-
       it 'causes that player to win, and the game to end' do
         board = game_board.instance_variable_get(:@board)
         symbol = game_board.instance_variable_get(:@player_one_symbol)
@@ -99,12 +96,11 @@ end
         board[0][0] = symbol
         board[0][1] = symbol
         board[0][2] = symbol
-        expect{game_board.winner?(name, symbol, board)}.to output(winner_message).to_stdout
+        expect { game_board.winner?(name, symbol, board) }.to output(winner_message).to_stdout
       end
     end
 
     context 'when a user has completed a vertical row of three symbols' do
-
       it 'causes that player to win, and the game to end' do
         board = game_board.instance_variable_get(:@board)
         symbol = game_board.instance_variable_get(:@player_two_symbol)
@@ -113,12 +109,11 @@ end
         board[0][0] = symbol
         board[1][0] = symbol
         board[2][0] = symbol
-        expect{game_board.winner?(name, symbol, board)}.to output(winner_message).to_stdout
+        expect { game_board.winner?(name, symbol, board) }.to output(winner_message).to_stdout
       end
     end
 
     context 'when a user has completed a diagonal row of three symbols' do
-
       it 'causes that player to win, and the game to end' do
         board = game_board.instance_variable_get(:@board)
         symbol = game_board.instance_variable_get(:@player_one_symbol)
@@ -127,11 +122,8 @@ end
         board[0][0] = symbol
         board[1][1] = symbol
         board[2][2] = symbol
-        expect{game_board.winner?(name, symbol, board)}.to output(winner_message).to_stdout
+        expect { game_board.winner?(name, symbol, board) }.to output(winner_message).to_stdout
       end
     end
   end
-
 end
-        
-        
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,54 +1,53 @@
 class Player
   attr_reader :name, :sybmol
+
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
   end
 
   def select_row
-    puts "#{self.name} it's your turn! Please input your desired row (1-3) and press enter"
+    puts "#{name} it's your turn! Please input your desired row (1-3) and press enter"
     row = gets.to_i
-      if row < 4 && row > 0
-        puts "great choice #{self.name}, you have selected #{row}"
-        row -= 1 
-      else
-        puts "that's an invalid choice #{self.name}, please input a number between 1-3"
-        select_row
-      end
-      row
+    if row < 4 && row > 0
+      puts "great choice #{name}, you have selected #{row}"
+      row -= 1
+    else
+      puts "that's an invalid choice #{name}, please input a number between 1-3"
+      select_row
+    end
+    row
   end
 
   def select_column
-    puts "#{self.name} it's your turn! Please input your desired column (1-3) and press enter"
+    puts "#{name} it's your turn! Please input your desired column (1-3) and press enter"
     column = gets.to_i
-      if column < 4 && column > 0
-        puts "great choice #{self.name}, you have selected #{column}"
-        column -= 1 
-      else
-        puts "that's an invalid choice #{self.name}, please input a number between 1-3"
-        select_column
-      end
-      column
+    if column < 4 && column > 0
+      puts "great choice #{name}, you have selected #{column}"
+      column -= 1
+    else
+      puts "that's an invalid choice #{name}, please input a number between 1-3"
+      select_column
+    end
+    column
   end
-
 end
 
 class TicTacToe
   attr_reader :player_one, :player_two, :player_one_name, :player_one_symbol, :player_two_name, :player_two_symbol
   attr_accessor :board
-  
-  def initialize (player_one_name = 'Johnny', player_one_symbol = 'X', player_two_name = 'Bob', player_two_symbol = 'O')
+
+  def initialize(player_one_name = 'Johnny', player_one_symbol = 'X', player_two_name = 'Bob', player_two_symbol = 'O')
     @player_one_name = player_one_name
     @player_one_symbol = player_one_symbol
     @player_two_name = player_two_name
     @player_two_symbol = player_two_symbol
     @player_one = Player.new(player_one_name, player_one_symbol)
     @player_two = Player.new(player_two_name, player_two_symbol)
-    @board = Array.new(3) {Array.new(3)}
+    @board = Array.new(3) { Array.new(3) }
   end
 
-  def place_symbol(row, column, symbol, board = @board) 
-   
+  def place_symbol(row, column, symbol, board = @board)
     if board[row][column].nil?
       board[row][column] = symbol
       puts "#{board}"
@@ -60,8 +59,7 @@ class TicTacToe
   end
 
   def winner?(name, symbol, board = @board)
-      if 
-       board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol ||
+    if board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol ||
        board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol ||
        board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol ||
        board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol ||
@@ -69,9 +67,9 @@ class TicTacToe
        board[0][2] == symbol && board[1][2] == symbol && board[2][2] == symbol ||
        board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol ||
        board[2][0] == symbol && board[1][1] == symbol && board[0][2] == symbol
-       puts "Congratulations #{name}, You have won!"
-       true
-      end
+      puts "Congratulations #{name}, You have won!"
+      true
+    end
   end
 
   def player_one_turn
@@ -81,8 +79,8 @@ class TicTacToe
     if place_symbol(row, column, symbol) == false
       player_one_turn
     else
-    name = @player_one_name
-    winner?(name, symbol)
+      name = @player_one_name
+      winner?(name, symbol)
     end
   end
 
@@ -93,36 +91,36 @@ class TicTacToe
     if place_symbol(row, column, symbol) == false
       player_two_turn
     else
-    name = @player_two_name
-    winner?(name, symbol)
+      name = @player_two_name
+      winner?(name, symbol)
     end
   end
-  
+
   def play
     i = 0
     loop do
-    if player_one_turn == true
-      break
-    else
-      i += 1
-    end
-
-    if i == 9
-      puts "This game has tied, please restart if you would like to play again!"
-      break
-    end
-    
-    if player_two_turn == true
-      break
-    else
-      i += 1
-    end
-
-    if i == 9
-      puts "This game has tied, please restart if you would like to play again!"
-      break
+      if player_one_turn == true
+        break
+      else
+        i += 1
       end
-    end 
+
+      if i == 9
+        puts 'This game has tied, please restart if you would like to play again!'
+        break
+      end
+
+      if player_two_turn == true
+        break
+      else
+        i += 1
+      end
+
+      if i == 9
+        puts 'This game has tied, please restart if you would like to play again!'
+        break
+      end
+    end
   end
 end
 
